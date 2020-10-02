f74fa5b34ef9f6cd2f637042ecae59c9fd8d4677
Malte Mues <mail.mues@gmail.com>
2020-10-02 12:22:08 +0200

    Adding configuration Infrastructure for the String modell

--- src/peers/gov/nasa/jpf/jdart/peers/strings/SMTLibStringBuilderModel.java
+++ src/peers/gov/nasa/jpf/jdart/peers/strings/SMTLibStringBuilderModel.java
@@ -1,9 +1,20 @@
 package gov.nasa.jpf.jdart.peers.strings;
 
 import gov.nasa.jpf.annotation.MJI;
+import gov.nasa.jpf.constraints.api.Expression;
+import gov.nasa.jpf.constraints.api.Variable;
+import gov.nasa.jpf.constraints.expressions.Constant;
+import gov.nasa.jpf.constraints.expressions.StringCompoundExpression;
+import gov.nasa.jpf.constraints.types.BuiltinTypes;
+import gov.nasa.jpf.jdart.ConcolicMethodExplorer;
+import gov.nasa.jpf.jdart.ConcolicUtil;
 import gov.nasa.jpf.jdart.annotations.SymbolicPeer;
+import gov.nasa.jpf.jdart.objects.SymbolicSMTString;
 import gov.nasa.jpf.jdart.peers.JPF_java_lang_StringBuilder;
+import gov.nasa.jpf.vm.ElementInfo;
 import gov.nasa.jpf.vm.MJIEnv;
+
+import java.util.ArrayList;
 
 public class SMTLibStringBuilderModel {
 
@@ -18,22 +29,88 @@ public class SMTLibStringBuilderModel {
 	/*
 	 * gets always implicitly called by string concatenation via + operator
 	 */ public int append__Ljava_lang_String_2__Ljava_lang_StringBuilder_2(MJIEnv env, int objref, int sref) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+		ElementInfo ei = env.getElementInfo(sref);
+		if (ei == null || ei.isNull()) {
+			return peer.super_append__Ljava_lang_String_2__Ljava_lang_StringBuilder_2(env, objref, sref);
+		}
+
+		SymbolicSMTString symbParam = ei.getObjectAttr(SymbolicSMTString.class);
+
+		ElementInfo eiSB = env.getElementInfo(objref);
+		SymbolicSMTString symbSB = eiSB.getObjectAttr(SymbolicSMTString.class);
+
+		if (symbSB == null && symbParam == null) {
+			return peer.super_append__Ljava_lang_String_2__Ljava_lang_StringBuilder_2(env, objref, sref);
+		}
+
+		if (symbSB == null && symbParam != null) {
+
+			int field = env.getReferenceField(objref, "value");
+			char[] values = env.getCharArrayObject(field);
+			ArrayList<Expression> symbolicChars = new ArrayList<>();
+			int count = env.getIntField(objref, "count");
+
+			ConcolicUtil.Pair<String> newSymbolicString = ca.getOrCreateSymbolicString();
+			String value = new String(values, 0, count);
+			symbSB =
+					new SymbolicSMTString((Variable<String>) newSymbolicString.symb, Constant.create(BuiltinTypes.STRING,
+																																													 value));
+		}
+
+		int concreteResultRef = peer.super_append__Ljava_lang_String_2__Ljava_lang_StringBuilder_2(env, objref, sref);
+
+		//test for symbolic string
+		if (symbParam != null) {
+
+
+			Expression<String> concat = StringCompoundExpression.createConcat(symbSB.symbolicValue, symbParam.symbolicValue);
+			ConcolicUtil.Pair<String> newSymbolicString = ca.getOrCreateSymbolicString();
+			symbSB = new SymbolicSMTString((Variable<String>) newSymbolicString.symb, concat);
+			env.addObjectAttr(concreteResultRef, symbSB);
+		}
+		return concreteResultRef;
 	}
 
 	@MJI
 	@SymbolicPeer
 	public char charAt__I__C(MJIEnv env, int objref, int index) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		throw new UnsupportedOperationException();
 	}
 
 	@MJI
 	@SymbolicPeer
 	public void setCharAt__IC__V(MJIEnv env, int objref, int index, char character) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		throw new UnsupportedOperationException();
 	}
 
 	public void getChars__II_3CI__V(MJIEnv env, int objref, int start, int end, int dest, int dest_start) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ElementInfo eiSB = env.getElementInfo(objref);
+		ElementInfo eiCA = env.getElementInfo(dest);
+		int maxSize = env.getIntField(objref, "count");
+		int charArrayRef = env.getReferenceField(objref, "value");
+		char[] values = env.getCharArrayObject(charArrayRef);
+		char[] destChar = eiCA.asCharArray();
+		if (end > maxSize || start < 0 || dest_start < 0 || start > end || (dest_start + end - start) > destChar.length) {
+			env.throwException("java.lang.IndexOutOfBoundsException");
+		} else {
+			for (int i = 0; i + start < end; i++) {
+				destChar[dest_start + i] = values[i + start];
+			}
+		}
+		SymbolicSMTString symbolic = eiSB.getObjectAttr(SymbolicSMTString.class);
+		ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+		if (symbolic != null) {
+			SymbolicSMTString[] symbolicChars = new SymbolicSMTString[dest_start + end - start];
+			for (int i = 0; i + start < end; i++) {
+				ConcolicUtil.Pair<String> newChar = ca.getOrCreateSymbolicString();
+				symbolicChars[dest_start + i] = new SymbolicSMTString((Variable<String>) newChar.symb,
+																															StringCompoundExpression.createAt(symbolic.symbolicValue,
+																																																Constant.create(
+																																																		BuiltinTypes.SINT32,
+																																																		i + start)));
+			}
+			eiCA.addObjectAttr(symbolicChars);
+		}
 	}
 }
--- src/peers/gov/nasa/jpf/jdart/peers/strings/SMTLibStringModel.java
+++ src/peers/gov/nasa/jpf/jdart/peers/strings/SMTLibStringModel.java
@@ -17,10 +17,27 @@
 package gov.nasa.jpf.jdart.peers.strings;
 
 import gov.nasa.jpf.annotation.MJI;
+import gov.nasa.jpf.constraints.api.Expression;
+import gov.nasa.jpf.constraints.api.Variable;
+import gov.nasa.jpf.constraints.expressions.Constant;
+import gov.nasa.jpf.constraints.expressions.Negation;
+import gov.nasa.jpf.constraints.expressions.NumericBooleanExpression;
+import gov.nasa.jpf.constraints.expressions.NumericComparator;
+import gov.nasa.jpf.constraints.expressions.NumericCompound;
+import gov.nasa.jpf.constraints.expressions.NumericOperator;
+import gov.nasa.jpf.constraints.expressions.StringBooleanExpression;
+import gov.nasa.jpf.constraints.expressions.StringCompoundExpression;
+import gov.nasa.jpf.constraints.expressions.StringIntegerExpression;
+import gov.nasa.jpf.constraints.types.BuiltinTypes;
+import gov.nasa.jpf.constraints.util.ExpressionUtil;
 import gov.nasa.jpf.jdart.ConcolicMethodExplorer;
+import gov.nasa.jpf.jdart.ConcolicUtil;
 import gov.nasa.jpf.jdart.annotations.SymbolicPeer;
+import gov.nasa.jpf.jdart.objects.SymbolicNumber;
 import gov.nasa.jpf.jdart.objects.SymbolicSMTString;
 import gov.nasa.jpf.jdart.peers.JPF_java_lang_String;
+import gov.nasa.jpf.vm.DynamicElementInfo;
+import gov.nasa.jpf.vm.ElementInfo;
 import gov.nasa.jpf.vm.MJIEnv;
 import gov.nasa.jpf.vm.ThreadInfo;
 
@@ -35,31 +52,185 @@ public class SMTLibStringModel {
 	@MJI
 	@SymbolicPeer
 	public int init___3BII__Ljava_lang_String_2(MJIEnv env, int objRef, int bytesRef, int offset, int length) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ElementInfo ei = env.getElementInfo(bytesRef);
+		Expression symbolicLength = ei.getObjectAttr(Expression.class);
+		ConcolicUtil.Pair<String> aNewString =
+				ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo()).getOrCreateSymbolicString();
+		StringIntegerExpression newLength = StringIntegerExpression.createLength(aNewString.symb);
+		NumericBooleanExpression constrainedLength =
+				NumericBooleanExpression.create(symbolicLength, NumericComparator.EQ, newLength);
+
+		for (int i = 0; i < ei.arrayLength(); i++) {
+			Expression charExpr = ei.getElementAttr(i, Expression.class);
+			if (charExpr != null && !(charExpr instanceof Variable)) {
+				throw new UnsupportedOperationException("It seems like this char is constrained");
+			}
+		}
+		env.setObjectAttr(objRef, new SymbolicSMTString((Variable<String>) aNewString.symb, constrainedLength));
+		return peer.super_init___3BII(env, objRef, bytesRef, offset, length);
 	}
 
 	@MJI
 	@SymbolicPeer
 	public int length____I(MJIEnv env, int objRef) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ElementInfo ei = env.getElementInfo(objRef);
+		SymbolicSMTString symb = ei.getObjectAttr(SymbolicSMTString.class);
+		if (symb != null) {
+			StringIntegerExpression strLength = StringIntegerExpression.createLength(symb.strVar);
+			env.setReturnAttribute(strLength);
+		}
+		String concreteString = ei.asString();
+		return concreteString.length();
 	}
 
 	@MJI
 	@SymbolicPeer
 	public char charAt__I__C(MJIEnv env, int objRef, int index) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ThreadInfo ti = env.getThreadInfo();
+		ConcolicMethodExplorer analysis = ConcolicMethodExplorer.getCurrentAnalysis(ti);
+
+		String eiThis = env.getElementInfo(objRef).asString();
+		SymbolicSMTString symb = env.getObjectAttr(objRef, SymbolicSMTString.class);
+		Expression symbIndex = null;
+
+		if (env.getArgAttributes() != null) {
+			symbIndex = (Expression) env.getArgAttributes()[1];
+		}
+		if (analysis == null || (symb == null && symbIndex == null)) {
+			return peer.super_charAt__I__C(env, objRef, index);
+		}
+		Expression length = (symb != null) ? StringIntegerExpression.createLength(symb.strVar) : null;
+		if (length == null) {
+			length = new Constant(BuiltinTypes.SINT32, eiThis.length());
+		}
+		if (symbIndex == null) {
+			symbIndex = new Constant(BuiltinTypes.SINT32, index);
+		}
+		Expression constraint = ExpressionUtil.and(new NumericBooleanExpression(new Constant<>(BuiltinTypes.SINT32, 0),
+																																						NumericComparator.LE,
+																																						symbIndex),
+																							 new NumericBooleanExpression(symbIndex, NumericComparator.LT, length));
+
+		boolean sat = (0 <= index) && (index < eiThis.length());
+		int branchIdx = sat ? 0 : 1;
+		analysis.decision(ti, null, branchIdx, constraint, new Negation(constraint));
+
+		try {
+			if (symb == null) {
+				throw new UnsupportedOperationException("We need to compute a valueSet in this case");
+			}
+
+			StringCompoundExpression charAt = StringCompoundExpression.createAt(symb.strVar, symbIndex);
+			if (symb.symbolicValue instanceof StringCompoundExpression) {
+				charAt = StringCompoundExpression.createAt(symb.symbolicValue, symbIndex);
+			}
+			Variable<String> newStrVar = (Variable<String>) analysis.getOrCreateSymbolicString().symb;
+			StringBooleanExpression assignment = StringBooleanExpression.createEquals(newStrVar, charAt);
+			env.setReturnAttribute(new SymbolicSMTString(newStrVar, assignment));
+			return peer.charAt__I__C(env, objRef, index);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			env.throwException(ArrayIndexOutOfBoundsException.class.getName(), e.getMessage());
+			return MJIEnv.NULL;
+		}
 	}
 
 	@MJI
 	@SymbolicPeer
 	public boolean equals__Ljava_lang_Object_2__Z(MJIEnv env, int objRef, int argRef) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		if (argRef == env.NULL) {
+			return false;
+		}
+
+		ThreadInfo ti = env.getThreadInfo();
+		ConcolicMethodExplorer analysis = ConcolicMethodExplorer.getCurrentAnalysis(ti);
+		System.out.println("----- Symbolic String equals");
+		SymbolicSMTString symbThis = env.getObjectAttr(objRef, SymbolicSMTString.class);
+		SymbolicSMTString symbOther = env.getObjectAttr(argRef, SymbolicSMTString.class);
+
+		if (analysis == null || (symbThis == null && symbOther == null)) {
+			System.out.println("----- concrete, " + (analysis == null));
+			return peer.super_equals__Ljava_lang_Object_2__Z(env, objRef, argRef);
+		}
+		System.out.println("----- symbolic");
+
+		if (symbThis != null && symbOther != null) {
+			System.out.println("----- both symbolic");
+			String selfConcrete = env.getStringObject(objRef);
+			String otherConcrete = env.getStringObject(argRef);
+
+			Expression thisSymbValue =
+					symbThis.symbolicValue instanceof StringCompoundExpression ? symbThis.symbolicValue : symbThis.strVar;
+			Expression otherSymbValue =
+					symbOther.symbolicValue instanceof StringCompoundExpression ? symbOther.symbolicValue : symbOther.strVar;
+			Expression<Boolean> decisionExpr = StringBooleanExpression.createEquals(thisSymbValue, otherSymbValue);
+			boolean concreteEquals = selfConcrete.equals(otherConcrete);
+			analysis.decision(ti, null, concreteEquals ? 0 : 1, decisionExpr, Negation.create(decisionExpr));
+			return concreteEquals;
+		}
+
+		System.out.println("----- one symbolic!");
+
+		SymbolicSMTString symb = (symbThis != null) ? symbThis : symbOther;
+		if (symb.isFromNumber()) {
+			return peer.equals_numbers(env, (symbThis != null) ? objRef : argRef, (symbThis != null) ? argRef : objRef);
+		}
+
+		int concRef = (symbThis != null) ? argRef : objRef;
+		int symbRef = (symbThis != null) ? objRef : argRef;
+
+		ElementInfo eiConc = ti.getElementInfo(concRef);
+		System.out.println("The concret String is: " + eiConc.asString());
+
+		ElementInfo eiSymb = ti.getElementInfo(symbRef);
+		String concString = eiConc.asString();
+		String symbString = eiSymb.asString();
+
+		Expression symbolicValue =
+				symb.symbolicValue instanceof StringCompoundExpression ? symb.symbolicValue : symb.strVar;
+
+		Expression equals =
+				StringBooleanExpression.createEquals(symbolicValue, Constant.create(BuiltinTypes.STRING, concString));
+		boolean sat = peer.super_equals__Ljava_lang_Object_2__Z(env, objRef, argRef);
+		System.out.println("----- equals: " + sat);
+		int branchIdx = sat ? 0 : 1;
+		analysis.decision(ti, null, branchIdx, equals, Negation.create(equals));
+		return sat;
 	}
 
 	@MJI
 	@SymbolicPeer
 	public int concat__Ljava_lang_String_2__Ljava_lang_String_2(MJIEnv env, int objRef, int paramRef) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ElementInfo ei1 = env.getElementInfo(objRef);
+		ElementInfo ei2 = env.getElementInfo(paramRef);
+		String concreteS1 = ei1.asString();
+		String concreteS2 = ei2.asString();
+
+		int newRes = env.newString(concreteS1.concat(concreteS2));
+		SymbolicSMTString s1 = ei1.getObjectAttr(SymbolicSMTString.class);
+		SymbolicSMTString s2 = ei2.getObjectAttr(SymbolicSMTString.class);
+
+
+		Expression smtValueS1 = Variable.create(BuiltinTypes.STRING, concreteS1);
+		Expression smtValueS2 = Variable.create(BuiltinTypes.STRING, concreteS2);
+
+		if (s1 != null) {
+			smtValueS1 = s1.symbolicValue instanceof StringCompoundExpression ? s1.symbolicValue : s1.strVar;
+		}
+		if (s2 != null) {
+			smtValueS2 = s2.symbolicValue instanceof StringCompoundExpression ? s2.symbolicValue : s2.strVar;
+		}
+		if (s1 != null || s2 != null) {
+			ThreadInfo ti = env.getThreadInfo();
+			ConcolicMethodExplorer analysis = ConcolicMethodExplorer.getCurrentAnalysis(ti);
+			ConcolicUtil.Pair<String> newStringVar = analysis.getOrCreateSymbolicString();
+			SymbolicSMTString symbolicSMTString = new SymbolicSMTString((Variable<String>) newStringVar.symb,
+																																	StringCompoundExpression.createConcat(smtValueS1,
+																																																				smtValueS2));
+			ElementInfo newEI = env.getElementInfo(newRes);
+			newEI.addObjectAttr(symbolicSMTString);
+		}
+		return newRes;
 	}
 
 	@MJI
@@ -77,7 +248,32 @@ public class SMTLibStringModel {
 	@MJI
 	@SymbolicPeer
 	public boolean contains__Ljava_lang_CharSequence_2__Z(MJIEnv env, int objRef, int charSequenceRef) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		ThreadInfo ti = env.getThreadInfo();
+
+		String self = env.getStringObject(objRef);
+		String other = "";
+		if (((DynamicElementInfo) env.getHeap().get(charSequenceRef)).getClassInfo()
+																																 .getName()
+																																 .startsWith("java.lang.String")) {
+			other = env.getStringObject(charSequenceRef);
+		} else {
+			ConcolicMethodExplorer.getCurrentAnalysis(ti).completePathDontKnow(ti);
+			return false;
+		}
+		ElementInfo eiSelf = env.getElementInfo(objRef);
+		ElementInfo eiOther = env.getElementInfo(charSequenceRef);
+		SymbolicSMTString symbolicSelf = eiSelf.getObjectAttr(SymbolicSMTString.class);
+		SymbolicSMTString symbolicOther = eiOther.getObjectAttr(SymbolicSMTString.class);
+		ConcolicMethodExplorer analysis = ConcolicMethodExplorer.getCurrentAnalysis(ti);
+		if (analysis == null || (symbolicOther == null && symbolicSelf == null)) {
+			return self.contains(other);
+		} else if (symbolicOther != null && symbolicSelf == null) {
+			return evaluateContainsConcreteSelfSymbolicOther(self, other, symbolicOther, analysis, ti);
+		} else if (symbolicOther == null && symbolicSelf != null) {
+			return evaluateContainsSymbolicSelfConcreteOther(self, symbolicSelf, other, analysis, ti);
+		} else {
+			return evaluateContainsSymbolicSelfSymbolicOther(self, symbolicSelf, other, symbolicOther, analysis, ti);
+		}
 	}
 
 	private boolean evaluateContainsConcreteSelfSymbolicOther(String self,
@@ -85,8 +281,11 @@ public class SMTLibStringModel {
 																														SymbolicSMTString symbolicOther,
 																														ConcolicMethodExplorer analysis,
 																														ThreadInfo ti) {
-
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		boolean ret = self.contains(other);
+		Constant selfStr = Constant.create(BuiltinTypes.STRING, self);
+		StringBooleanExpression containsExpr = StringBooleanExpression.createContains(selfStr, symbolicOther.strVar);
+		analysis.decision(ti, null, ret ? 0 : 1, containsExpr, Negation.create(containsExpr));
+		return ret;
 	}
 
 	private boolean evaluateContainsSymbolicSelfConcreteOther(String self,
@@ -94,7 +293,11 @@ public class SMTLibStringModel {
 																														String other,
 																														ConcolicMethodExplorer analysis,
 																														ThreadInfo ti) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		boolean ret = self.contains(other);
+		Constant otherStr = Constant.create(BuiltinTypes.STRING, other);
+		StringBooleanExpression containsExpr = StringBooleanExpression.createContains(symbolicSelf.strVar, otherStr);
+		analysis.decision(ti, null, ret ? 0 : 1, containsExpr, Negation.create(containsExpr));
+		return ret;
 	}
 
 	private boolean evaluateContainsSymbolicSelfSymbolicOther(String self,
@@ -103,30 +306,123 @@ public class SMTLibStringModel {
 																														SymbolicSMTString symbolicOther,
 																														ConcolicMethodExplorer analysis,
 																														ThreadInfo ti) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		boolean ret = self.contains(other);
+		StringBooleanExpression containsExpr =
+				StringBooleanExpression.createContains(symbolicSelf.strVar, symbolicOther.strVar);
+		analysis.decision(ti, null, ret ? 0 : 1, containsExpr, Negation.create(containsExpr));
+		return ret;
 	}
 
 
 	@MJI
 	public static int local_valueOf__D__Ljava_lang_String_2(MJIEnv env, int objRef, double value) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		Object[] attrs = env.getArgAttributes();
+		String res = String.valueOf(value);
+		if (attrs != null) {
+			for (Object o : attrs) {
+				if (o instanceof SymbolicNumber) {
+					ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+					ConcolicUtil.Pair<String> expr = ca.getOrCreateSymbolicString();
+					Constant symbolicValue = Constant.create(BuiltinTypes.STRING, res);
+					SymbolicSMTString symbolicRes = new SymbolicSMTString((Variable<String>) expr.symb, symbolicValue);
+					symbolicRes.setIsFromNumber(true);
+					symbolicRes.setSymbolicNumber((SymbolicNumber) o);
+					env.setReturnAttribute(symbolicRes);
+				}
+			}
+		}
+		return env.newString(res);
 	}
 
 
 	@MJI
 	public int substring__I__Ljava_lang_String_2(MJIEnv env, int objRef, int beginIndex) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		SymbolicSMTString symbolicString = env.getObjectAttr(objRef, SymbolicSMTString.class);
+		int res = peer.super_substring__I__Ljava_lang_String_2(env, objRef, beginIndex);
+		if (env.getArgAttributes() != null && (env.getArgAttributes()[1] != null || env.getArgAttributes()[2] != null)) {
+			System.out.println("JPF_java_lang_String.substring__I__Ljava_lang_String_2");
+			System.out.println("Cannot deal with symbolic values in args");
+			env.throwException("error.Assume");
+			return -1;
+		}
+		if (symbolicString != null) {
+			Constant cBegin = Constant.create(BuiltinTypes.SINT32, beginIndex);
+			NumericCompound upperBound =
+					NumericCompound.create(StringIntegerExpression.createLength(symbolicString.symbolicValue),
+																 NumericOperator.MINUS,
+																 cBegin);
+			StringCompoundExpression expr =
+					StringCompoundExpression.createSubstring(symbolicString.symbolicValue, cBegin, upperBound);
+
+			ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+			ConcolicUtil.Pair<String> varName = ca.getOrCreateSymbolicString();
+			SymbolicSMTString symbolicValue = new SymbolicSMTString((Variable<String>) varName.symb, expr);
+			env.addObjectAttr(res, symbolicValue);
+		}
+		return res;
 	}
 
 	@MJI
 	@SymbolicPeer
 	public int substring__II__Ljava_lang_String_2(MJIEnv env, int objRef, int beginIndex, int endIndex) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		SymbolicSMTString symbolicString = env.getObjectAttr(objRef, SymbolicSMTString.class);
+		int res = peer.super_substring__II__Ljava_lang_String_2(env, objRef, beginIndex, endIndex);
+		if (env.getArgAttributes() != null && (env.getArgAttributes()[1] != null || env.getArgAttributes()[2] != null)) {
+			System.out.println("JPF_java_lang_String.substring__II__Ljava_lang_String_2");
+			System.out.println("Cannot deal with symbolic values in args");
+			env.throwException("error.Assume");
+			return -1;
+		}
+		if (symbolicString != null) {
+			Constant cBegin = Constant.create(BuiltinTypes.SINT32, beginIndex);
+			Constant cEnd = Constant.create(BuiltinTypes.SINT32, endIndex);
+			StringCompoundExpression expr =
+					StringCompoundExpression.createSubstring(symbolicString.symbolicValue, cBegin, cEnd);
+
+			ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+			ConcolicUtil.Pair<String> varName = ca.getOrCreateSymbolicString();
+			SymbolicSMTString symbolicValue = new SymbolicSMTString((Variable<String>) varName.symb, expr);
+			env.addObjectAttr(res, symbolicValue);
+		}
+		return res;
 	}
 
 	@MJI
 	@SymbolicPeer
 	public int split__Ljava_lang_String_2___3Ljava_lang_String_2(MJIEnv env, int objRef, int rString0) {
-		throw new UnsupportedOperationException("SMTLib String Model not yet supported");
+		int concreteRes = peer.super_split__Ljava_lang_String_2___3Ljava_lang_String_2(env, objRef, rString0);
+		SymbolicSMTString symbolicString = env.getObjectAttr(objRef, SymbolicSMTString.class);
+		SymbolicSMTString symbolicParameter = env.getObjectAttr(rString0, SymbolicSMTString.class);
+		if (symbolicParameter != null) {
+			throw new UnsupportedOperationException("Cannot deal with symbolic parameter in String.split");
+		}
+		if (symbolicString != null) {
+			int[] fields = env.getReferenceArrayObject(concreteRes);
+			if (fields.length != 0) {
+				SymbolicSMTString[] symbolicFields = new SymbolicSMTString[fields.length];
+
+				String param = env.getStringObject(rString0);
+				Constant cParam = Constant.create(BuiltinTypes.STRING, param);
+				ConcolicMethodExplorer ca = ConcolicMethodExplorer.getCurrentAnalysis(env.getThreadInfo());
+				int count = 0;
+				for (int ref : fields) {
+					ConcolicUtil.Pair<String> newStringVar = ca.getOrCreateSymbolicString();
+					Expression value = Negation.create(StringBooleanExpression.createContains(newStringVar.symb, cParam));
+					ca.decision(env.getThreadInfo(), null, 0, value);
+					SymbolicSMTString field = new SymbolicSMTString((Variable<String>) newStringVar.symb, newStringVar.symb);
+					env.addObjectAttr(ref, field);
+					symbolicFields[count] = field;
+					++count;
+				}
+				Expression concat = symbolicFields[0].symbolicValue;
+				for (int i = 1; i < symbolicFields.length; i++) {
+					concat = StringCompoundExpression.createConcat(concat, cParam);
+					concat = StringCompoundExpression.createConcat(concat, symbolicFields[i].symbolicValue);
+				}
+				concat = StringBooleanExpression.createEquals(symbolicString.symbolicValue, concat);
+				ca.decision(env.getThreadInfo(), null, 0, concat);
+			}
+		}
+		return concreteRes;
 	}
 }
