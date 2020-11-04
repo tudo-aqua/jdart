4bd6388175668914bb1973842f0bf527726579ea
Falk Maria Howar <falk.howar@tu-dortmund.de>
2020-11-04 22:08:19 +0100

    disabled bc debug

--- src/main/gov/nasa/jpf/jdart/ConcolicInstructionFactory.java
+++ src/main/gov/nasa/jpf/jdart/ConcolicInstructionFactory.java
@@ -110,7 +110,7 @@ import gov.nasa.jpf.vm.NativeMethodInfo;
  */
 public class ConcolicInstructionFactory extends gov.nasa.jpf.jvm.bytecode.InstructionFactory {
 
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	public static JPFLogger logger = JPF.getLogger("jdart");
 
 
