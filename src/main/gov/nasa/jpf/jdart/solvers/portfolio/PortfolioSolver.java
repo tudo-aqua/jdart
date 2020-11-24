9a961858febd4b7b9d9768fea6ff306713f8ca96
Malte Mues <mail.mues@gmail.com>
2020-11-24 18:18:43 +0100

    Add a solver strategy and improved interaction time with CVC4

--- src/main/gov/nasa/jpf/jdart/solvers/portfolio/PortfolioSolver.java
+++ src/main/gov/nasa/jpf/jdart/solvers/portfolio/PortfolioSolver.java
@@ -6,6 +6,7 @@ import gov.nasa.jpf.constraints.api.SolverContext;
 import gov.nasa.jpf.constraints.api.Valuation;
 import gov.nasa.jpf.constraints.api.ValuationEntry;
 import gov.nasa.jpf.constraints.solvers.ConstraintSolverFactory;
+import gov.nasa.jpf.constraints.solvers.encapsulation.ProcessWrapperContext;
 import gov.nasa.jpf.constraints.solvers.encapsulation.ProcessWrapperSolver;
 import gov.nasa.jpf.constraints.solvers.encapsulation.SolvingResult;
 import java.util.LinkedList;
@@ -20,59 +21,93 @@ import java.util.concurrent.Future;
 
 public class PortfolioSolver extends ConstraintSolver {
 
-  private List<ConstraintSolver> solvers;
+  private List<ConstraintSolver> processWrappedSolvers;
+  private List<ConstraintSolver> directSolvers;
+  private ExecutorService exec;
+
 
   public PortfolioSolver(Properties properties) {
-    solvers = resolveSolvers(properties);
+    processWrappedSolvers = new LinkedList();
+    directSolvers = new LinkedList<>();
+    resolveSolvers(properties);
+    exec = Executors.newFixedThreadPool(processWrappedSolvers.size());
   }
 
   @Override
   public Result solve(Expression<Boolean> expression, Valuation valuation) {
+    StringOrFloatExpressionVisitor visitor = new StringOrFloatExpressionVisitor();
+    boolean isStringOrFloatExpression = expression.accept(visitor, null);
+    if (isStringOrFloatExpression) {
+      List<ConstraintSolver> solvers = new LinkedList<>();
+      for (ConstraintSolver solver : processWrappedSolvers) {
+        if (solver.getName().equalsIgnoreCase("cvc4")) {
+          solvers.add(solver);
+        }
+      }
+      return dispatcheProcessWrappedSolvers(expression, valuation, solvers);
+    } else {
+      for (ConstraintSolver solver : directSolvers) {
+        if (solver.getName().equalsIgnoreCase("z3")) {
+          return solver.solve(expression, valuation);
+        }
+      }
+    }
+    throw new IllegalArgumentException("Cannot run the problem with the provided solvers");
+  }
+
+
+  @Override
+  public SolverContext createContext() {
+    Map<String, String> env = System.getenv();
+    List<ProcessWrapperContext> ctxs = new LinkedList<>();
+    for (ConstraintSolver s : processWrappedSolvers) {
+      ctxs.add((ProcessWrapperContext) s.createContext());
+    }
+    List<SolverContext> direct = new LinkedList<>();
+    for (ConstraintSolver s : directSolvers) {
+      direct.add(s.createContext());
+    }
+    return new PortfolioSolverContext(ctxs, direct);
+  }
+
+  private Result dispatcheProcessWrappedSolvers(Expression<Boolean> expression,
+      Valuation valuation,
+      List<ConstraintSolver> solvers) {
     List<Runnable> calls = new LinkedList<>();
-    ExecutorService exec = Executors.newFixedThreadPool(solvers.size());
     ExecutorCompletionService ecs = new ExecutorCompletionService(exec);
-    for(ConstraintSolver solver: solvers){
+    for (ConstraintSolver solver : solvers) {
       ecs.submit(() -> {
         Valuation val = new Valuation();
         Result res = solver.solve(expression, val);
         return new SolvingResult(res, val);
       });
     }
-    return processResult(solvers.size(), ecs, valuation, exec);
+    return processResult(solvers.size(), ecs, valuation);
   }
 
-  @Override
-  public SolverContext createContext() {
-    Map<String, String> env = System.getenv();
-    List<SolverContext> ctxs = new LinkedList<>();
-    for(ConstraintSolver s: solvers){
-      ctxs.add(s.createContext());
-    }
-    return new PortfolioSolverContext(ctxs);
-  }
-
-  private List<ConstraintSolver> resolveSolvers(Properties properties){
+  private void resolveSolvers(Properties properties) {
     String specifiedSolvers = properties.getProperty("jdart.portfolio.solvers", "");
-    LinkedList<ConstraintSolver> solvers = new LinkedList();
     ConstraintSolverFactory csf = ConstraintSolverFactory.getRootFactory();
     String[] solverNames = specifiedSolvers.split(",");
-    for(String solverName: solverNames){
-      solvers.push(new ProcessWrapperSolver(solverName));
+    for (String solverName : solverNames) {
+      processWrappedSolvers.add(new ProcessWrapperSolver(solverName));
+      if (!solverName.equalsIgnoreCase("cvc4")) {
+        directSolvers.add(csf.createSolver(solverName));
+      }
     }
-    return solvers;
   }
 
-  public static Result processResult(int maxSolvers, ExecutorCompletionService ecs, Valuation valuation, ExecutorService exec){
-    for(int i = 0; i < maxSolvers; i++){
+  public static Result processResult(int maxSolvers, ExecutorCompletionService ecs,
+      Valuation valuation) {
+    for (int i = 0; i < maxSolvers; i++) {
       try {
         Future<SolvingResult> solverRes = ecs.take();
-        if(solverRes.isDone() && !solverRes.get().getResult().equals(Result.DONT_KNOW)){
-          if(solverRes.get().getResult().equals(Result.SAT)){
-            for(ValuationEntry e: solverRes.get().getVal().entries()){
+        if (solverRes.isDone() && !solverRes.get().getResult().equals(Result.DONT_KNOW)) {
+          if (solverRes.get().getResult().equals(Result.SAT)) {
+            for (ValuationEntry e : solverRes.get().getVal().entries()) {
               valuation.addEntry(e);
             }
           }
-          exec.shutdownNow();
           return solverRes.get().getResult();
         }
       } catch (InterruptedException e) {
@@ -81,7 +116,6 @@ public class PortfolioSolver extends ConstraintSolver {
         e.printStackTrace();
       }
     }
-    exec.shutdown();
     return Result.DONT_KNOW;
   }
 }
--- src/main/gov/nasa/jpf/jdart/solvers/portfolio/PortfolioSolverContext.java
+++ src/main/gov/nasa/jpf/jdart/solvers/portfolio/PortfolioSolverContext.java
@@ -4,6 +4,7 @@ import gov.nasa.jpf.constraints.api.ConstraintSolver.Result;
 import gov.nasa.jpf.constraints.api.Expression;
 import gov.nasa.jpf.constraints.api.SolverContext;
 import gov.nasa.jpf.constraints.api.Valuation;
+import gov.nasa.jpf.constraints.solvers.encapsulation.ProcessWrapperContext;
 import gov.nasa.jpf.constraints.solvers.encapsulation.SolvingResult;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,51 +13,94 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class PortfolioSolverContext extends SolverContext {
-  private List<SolverContext> solvers;
 
-  public PortfolioSolverContext(List<SolverContext> ctxs){
+  private List<ProcessWrapperContext> solvers;
+  private List<SolverContext> direct;
+  private ExecutorService exec;
+
+  public PortfolioSolverContext(List<ProcessWrapperContext> ctxs, List<SolverContext> direct) {
     this.solvers = ctxs;
+    this.direct = direct;
+    this.exec = Executors.newFixedThreadPool(ctxs.size());
   }
 
   @Override
   public void push() {
-    for(SolverContext ctx: solvers){
+    for (SolverContext ctx : solvers) {
+      ctx.push();
+    }
+    for (SolverContext ctx : direct) {
       ctx.push();
     }
   }
 
   @Override
   public void pop(int i) {
-    for(SolverContext ctx: solvers){
+    for (SolverContext ctx : solvers) {
+      ctx.pop(i);
+    }
+    for (SolverContext ctx : direct) {
       ctx.pop(i);
     }
   }
 
   @Override
   public Result solve(Valuation valuation) {
+    ProcessWrapperContext ctx = solvers.get(0);
+    Expression expression = ctx.getCurrentExpression();
+    StringOrFloatExpressionVisitor visitor = new StringOrFloatExpressionVisitor();
+    boolean isStringOrFloatExpression = (Boolean) expression.accept(visitor, null);
+    if (isStringOrFloatExpression) {
+      List<SolverContext> wrappedCtxs = new LinkedList<>();
+      for (ProcessWrapperContext solver : solvers) {
+        if (solver.getName().equalsIgnoreCase("cvc4")) {
+          wrappedCtxs.add(solver);
+        }
+      }
+      return dispatcheProcessWrappedSolvers(expression, valuation, wrappedCtxs);
+    } else {
+      if (direct.size() == 1) {
+        return direct.get(0).solve(valuation);
+      }
+    }
+    throw new IllegalArgumentException("Cannot run the problem with the provided solvers");
+  }
+
+  public Result dispatcheProcessWrappedSolvers(Expression<Boolean> expression,
+      Valuation valuation,
+      List<SolverContext> solvers) {
     List<Runnable> calls = new LinkedList<>();
-    ExecutorService exec = Executors.newFixedThreadPool(solvers.size());
     ExecutorCompletionService ecs = new ExecutorCompletionService(exec);
-    for(SolverContext solver: solvers){
+    for (SolverContext solver : solvers) {
       ecs.submit(() -> {
         Valuation val = new Valuation();
         Result res = solver.solve(val);
         return new SolvingResult(res, val);
       });
     }
-   return PortfolioSolver.processResult(solvers.size(), ecs, valuation, exec);
+    return PortfolioSolver.processResult(solvers.size(), ecs, valuation);
   }
 
   @Override
   public void add(List<Expression<Boolean>> list) {
-    for(SolverContext ctx: solvers){
+    for (SolverContext ctx : solvers) {
       ctx.add(list);
+    }
+    Expression expr = solvers.get(0).getCurrentExpression();
+    StringOrFloatExpressionVisitor visitor = new StringOrFloatExpressionVisitor();
+    if (!(Boolean) expr.accept(visitor, null)) {
+      for (SolverContext ctx : direct) {
+        ctx.add(list);
+      }
     }
   }
 
   @Override
   public void dispose() {
-    for(SolverContext ctx: solvers){
+    for (SolverContext ctx : solvers) {
+      ctx.dispose();
+    }
+    for (SolverContext ctx : direct) {
       ctx.dispose();
     }
   }
